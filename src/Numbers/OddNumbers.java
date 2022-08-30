48bca1896dca042c52d4319358f30ad1b86fd84c
jenniferali <jenniferoali@gmail.com>
2022-08-29 22:33:54 -0500

    code that prints even numbers 1-100 added

--- src/Numbers/OddNumbers.java
+++ src/Numbers/OddNumbers.java
@@ -2,8 +2,10 @@ package Numbers;
 
 public class OddNumbers {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i += 2) {
-                System.out.print(i+" ");
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
             }
         }
     }
+}
